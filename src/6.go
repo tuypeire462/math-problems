36fded05e465feca6724e0fb362626912888af14
tuypeire462 <tuypeire462@gmail.com>
2025-03-11 09:33:37 +0000

    Add 6.go

---
+++ src/6.go
@@ -0,0 +1,9 @@
+
+package main
+import "math"
+func main() {
+x := 5.0 // change this number to try different results!
+fmt.Println("The value of x is", x)
+y := math.Sqrt(x)
+fmt.Println("The square root of x is", y)
+}
