20e9230d044125a14954089faa4254ec5c3f6cdb
tuypeire462 <tuypeire462@gmail.com>
2025-03-29 18:49:25 +0000

    Add 19.go

---
+++ src/19.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+    // Define some values
+    var x, y int
+
+    // Perform simple operations on integers
+    fmt.Println("Result of 5 * 3 + 2:", 5*3+2)
+
+    // Perform arithmetic operations with float64 and integer types
+    x = 1.0 / 2.0
+    y = 2.0 + 3
+
+    // Print the results
+    fmt.Println("Result of", x, "divided by", y)
+}
